9b2a0b243a054ca897356c4b430b1996558bc743
Abdulmuqsit Adam <muqsitadam@gmail.com>
2022-05-11 22:28:59 +0100

    Fixed the component issue by importing react

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
-class App extends Component{
+class App extends React.Component{
   render(){
     return (
       <div className="App">
