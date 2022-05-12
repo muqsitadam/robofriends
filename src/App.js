3ae93333a7ede6fb6100a03070b529e033dc61a8
Abdulmuqsit Adam <muqsitadam@gmail.com>
2022-05-12 22:38:15 +0100

    Made some up to date corrections in App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import logo from './logo.svg';
-import React from 'react'
+import React, {Component} from 'react'
 import './App.css';
 
-class App extends React.Component{
+class App extends Component{
   render(){
     return (
       <div className="App">
