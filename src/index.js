ca38b9c8b292bca94d1d35ef8bdf69e33971fb30
Abdulmuqsit Adam <muqsitadam@gmail.com>
2022-05-12 22:55:53 +0100

    Initiated the use of props

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Hello />
+    <Hello greetings={"Hello" + " REacT NiNJa"}/>
   </React.StrictMode>
 );
 
