95c513dc74ca706322593627244f55089328d3ed
Abdulmuqsit Adam <muqsitadam@gmail.com>
2022-05-12 22:36:42 +0100

    Created my first react component

---
+++ src/Hello.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+import "./Hello.css"
+
+class Hello extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Hello world</h1>
+                <p>Welcome to my first react Component</p>
+            </div>
+        )
+    }
+}
+
+export default Hello;
