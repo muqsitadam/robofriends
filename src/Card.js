bb32ce5841bebde9828ca15c177961520d1a00d3
Abdulmuqsit Adam <muqsitadam@gmail.com>
2022-05-15 19:21:37 +0100

    Added the card component

---
+++ src/Card.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+
+const Card = () => {
+    return(
+        <div>
+            <img alt="robot" src="https://robohash.org/english?200*200"/>
+            <h2>John Doe</h2>
+            <p>john.doe@gmail.com</p>
+        </div>
+    )
+}
+
+export default Card;
