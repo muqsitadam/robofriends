6237a6cc3f46adacdc8dc21f6b23a79b50a73d6c
Abdulmuqsit Adam <muqsitadam@gmail.com>
2022-05-15 19:51:47 +0100

    Made dynamic robots cards

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,13 @@
 import React from "react"
+import "tachyons"
 
 
-const Card = () => {
+const Card = ({name, email, id}) => {
     return(
-        <div>
-            <img alt="robot" src="https://robohash.org/english?200*200"/>
-            <h2>John Doe</h2>
-            <p>john.doe@gmail.com</p>
+        <div className="tc bg-light-blue dib br3 pa3 ma2 grow bw3 shadow-5">
+            <img alt="robot" src={`https://robohash.org/${id}?200*200`}/>
+            <h2>{name}</h2>
+            <p>{email}</p>
         </div>
     )
 }
