92c37ac868a0140ad39171bd45d74e285cb8bffc
Abdulmuqsit Adam <muqsitadam@gmail.com>
2022-05-15 14:41:56 +0100

    Initiated the creation of card component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Hello from './Hello';
+import Card from './Card';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Hello greetings={"Hello" + " REacT NiNJa"}/>
+    <Card />
   </React.StrictMode>
 );
 
