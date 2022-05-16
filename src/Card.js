856ad6260838491aa236ff2e703e9d33e9930c47
Abdulmuqsit Adam <muqsitadam@gmail.com>
2022-05-16 22:57:16 +0100

    Learnt to use fragment

--- src/Card.js
+++ src/Card.js
@@ -1,14 +1,14 @@
-import React from "react"
+import React, {Fragment} from "react"
 import "tachyons"
 
 
 const Card = ({name, email, id}) => {
     return(
-        <div className="tc bg-light-blue dib br3 pa3 ma2 grow bw3 shadow-5">
+        <Fragment className="tc bg-light-blue dib br3 pa3 ma2 grow bw3 shadow-5">
             <img alt="robot" src={`https://robohash.org/${id}?200*200`}/>
             <h2>{name}</h2>
             <p>{email}</p>
-        </div>
+        </Fragment>
     )
 }
 
