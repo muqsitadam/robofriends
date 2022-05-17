4cb1d58eb22b2fec673f978b7f786af079849fa8
Abdulmuqsit Adam <muqsitadam@gmail.com>
2022-05-17 21:51:37 +0100

    Made the serach box functional

--- src/Card.js
+++ src/Card.js
@@ -4,7 +4,7 @@ import "tachyons"
 
 const Card = ({name, email, id}) => {
     return(
-        <div className="tc bg-light-blue dib br3 pa3 ma2 grow bw3 shadow-5">
+        <div className="tc bg-light-green dib br3 pa3 ma2 grow bw3 shadow-5">
             <img alt="robot" src={`https://robohash.org/${id}?200*200`}/>
             <h2>{name}</h2>
             <p>{email}</p>
