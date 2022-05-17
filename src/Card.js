3727fd9e15e2a9d8f0fb7eab4b4e8e10897d9859
Abdulmuqsit Adam <muqsitadam@gmail.com>
2022-05-17 19:37:11 +0100

    Replaced fragment with div to solve minor bug

--- src/Card.js
+++ src/Card.js
@@ -1,14 +1,14 @@
-import React, {Fragment} from "react"
+import React from "react"
 import "tachyons"
 
 
 const Card = ({name, email, id}) => {
     return(
-        <Fragment className="tc bg-light-blue dib br3 pa3 ma2 grow bw3 shadow-5">
+        <div className="tc bg-light-blue dib br3 pa3 ma2 grow bw3 shadow-5">
             <img alt="robot" src={`https://robohash.org/${id}?200*200`}/>
             <h2>{name}</h2>
             <p>{email}</p>
-        </Fragment>
+        </div>
     )
 }
 
