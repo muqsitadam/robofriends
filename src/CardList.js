3a5412608b714c0029738fdb4000e70b9645b63a
Abdulmuqsit Adam <muqsitadam@gmail.com>
2022-05-17 20:10:59 +0100

    Added the unique key for each array list

--- src/CardList.js
+++ src/CardList.js
@@ -6,6 +6,7 @@ return(
     <div>
         {robots.map((user, i) => {
         return  <Card 
+                key={robots[i].id}
                 id={robots[i].id} 
                 name={robots[i].name} 
                 email={robots[i].email} />
