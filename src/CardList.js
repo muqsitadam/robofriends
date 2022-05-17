8ffbf625382d106d610c883f8ea907a617734f68
Abdulmuqsit Adam <muqsitadam@gmail.com>
2022-05-17 20:02:15 +0100

    Made card lsit dynamic

---
+++ src/CardList.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Card from './Card'
+
+const cardList = ({robots}) => {
+return(
+    <div>
+        {robots.map((user, i) => {
+        return  <Card 
+                id={robots[i].id} 
+                name={robots[i].name} 
+                email={robots[i].email} />
+            })}
+    </div>
+)}
+
+export default cardList;
