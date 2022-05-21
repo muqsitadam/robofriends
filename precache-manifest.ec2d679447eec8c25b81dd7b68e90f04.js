23c4c0c1431583e2822ed975e93834d35a384695
Abdulmuqsit Adam <muqsitadam@gmail.com>
2022-05-21 17:45:32 +0100

    Updates

---
+++ precache-manifest.ec2d679447eec8c25b81dd7b68e90f04.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "2851227bd5626103e167",
+    "url": "/robofriends/static/css/main.46d6afb0.chunk.css"
+  },
+  {
+    "revision": "2851227bd5626103e167",
+    "url": "/robofriends/static/js/main.2851227b.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/robofriends/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "34392a72953a4bb6c54e",
+    "url": "/robofriends/static/css/2.3a1f8871.chunk.css"
+  },
+  {
+    "revision": "34392a72953a4bb6c54e",
+    "url": "/robofriends/static/js/2.34392a72.chunk.js"
+  },
+  {
+    "revision": "69d7087b8b0ba8fee442",
+    "url": "/robofriends/static/js/runtime~main.69d7087b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "db3db8de7c9d0020f79fd35eba80d746",
+    "url": "/robofriends/index.html"
+  }
+];
